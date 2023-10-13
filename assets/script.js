f0870692da697bd3d992cba6556f2a272e825dc2
meerkat501 <leonardosoto911@gmail.com>
2023-10-12 21:12:37 -0500

    add event listener function for the search bar

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
+document.getElementById('search-button').addEventListener('click', function () {
+    const searchTerm = document.getElementById('search-bar').value;
+    searchMarvelCharacter(searchTerm);
+    updateSearchHistory(searchTerm);
+});
 
+
+  
   
